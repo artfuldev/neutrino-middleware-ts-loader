b517fde0116c1b430e2fca27b8f8b5dfd1687dd0
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-04-22 21:00:20 +0530

    Resolve .ts and .tsx extensions

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-module.exports = ({ config }, options) => config.module
-  .rule('typescript')
-    .test(/\.tsx?$/)
-      .use('ts')
-        .loader(require.resolve('ts-loader'))
-        .when(options.ts, use => use.options(options.ts))
-        .end();
+module.exports = ({ config }, options) =>
+  config
+    .resolve
+      .extensions
+        .prepend('.ts')
+        .prepend('.tsx')
+        .end()
+      .end()
+    .module
+    .rule('typescript')
+      .test(/\.tsx?$/)
+        .use('ts')
+          .loader(require.resolve('ts-loader'))
+          .when(options.ts, use => use.options(options.ts))
+          .end();
