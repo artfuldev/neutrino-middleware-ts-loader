f1c2dd5d06e2d61948deeb3055dadb6657d09290
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-04-22 21:11:44 +0530

    Add source-map-loader

--- index.js
+++ index.js
@@ -4,12 +4,23 @@ module.exports = ({ config }, options) =>
       .extensions
         .prepend('.ts')
         .prepend('.tsx')
+        .prepend('.js')
         .end()
       .end()
     .module
-    .rule('typescript')
-      .test(/\.tsx?$/)
+      .rule('sourcemap')
+        .test(/\.js$/)
+        .pre()
+        .use('sourcemap')
+          .loader(require.resolve('source-map-loader'))
+          .when(options.sourcemap, use => use.options(options.sourcemap))
+          .end()
+        .end()
+      .rule('typescript')
+        .test(/\.tsx?$/)
         .use('ts')
           .loader(require.resolve('ts-loader'))
           .when(options.ts, use => use.options(options.ts))
-          .end();
+          .end()
+        .end()
+      .end();
